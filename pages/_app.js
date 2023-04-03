0d540b89b49d0f774a45093cbc10ebc3a050a715
Paul Scanlon <pauliescanlon@gmail.com>
2023-04-03 17:32:12 +0100

    feat: add tailwind

---
+++ pages/_app.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import Layout from '../components/layout';
+
+import '../styles/globals.css';
+
+const App = ({ Component, pageProps }) => {
+  return (
+    <Layout>
+      <Component {...pageProps} />
+    </Layout>
+  );
+};
+
+export default App;
---
+++ pages/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Page = () => {
+  return <h1>Demo Data Residency</h1>;
+};
+
+export default Page;
