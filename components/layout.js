1679dba22eac7118a80d8753f4bea28bceafe309
Paul Scanlon <pauliescanlon@gmail.com>
2023-04-04 09:46:00 +0100

    chore: add husky

--- components/layout.js
+++ components/layout.js
@@ -1,7 +1,12 @@
-import React from 'react';
+import React, { Fragment } from "react";
 
 const Layout = ({ children }) => {
-  return <main className='prose'>{children}</main>;
+  return (
+    <Fragment>
+      <header className="mx-auto p-4"></header>
+      <main className="prose max-w-7xl mx-auto px-4 sm:px-8">{children}</main>
+    </Fragment>
+  );
 };
 
 export default Layout;
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,14 @@
-import React from 'react';
+import React from "react";
+
+import CockroachLabsLogo from "../components/cockroach-labs-logo";
 
 const Page = () => {
-  return <h1>Demo Data Residency</h1>;
+  return (
+    <section>
+      <h1>Demo Data Residency</h1>
+      <CockroachLabsLogo color="brand-white" />
+    </section>
+  );
 };
 
 export default Page;
