5ce499769dac3391c68d489583b17aef56fedba6
Paul Scanlon <pauliescanlon@gmail.com>
2023-04-05 10:04:18 +0100

    feat: add silo-lockup to index page

---
+++ components/silo-lockup.js
@@ -0,0 +1,156 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+const SiloLockup = ({ className }) => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      x="0px"
+      y="0px"
+      viewBox="0 0 1600 800"
+      aria-label="Silo Lockup"
+      className={className}
+    >
+      <g>
+        <path className="fill-brand-electric-purple" d="M599.78-0.21h-200v200C399.78,89.33,489.32-0.21,599.78-0.21z" />
+        <path className="fill-brand-electric-purple" d="M599.78,124.79c-41.42,0-75,33.58-75,75h75V124.79z" />
+        <path
+          className="fill-brand-iridescent-blue"
+          d="M1399.61,199.79v-200h-200C1310.07-0.21,1399.61,89.33,1399.61,199.79z"
+        />
+        <path className="fill-brand-iridescent-blue" d="M1274.61,199.79c0-41.42-33.58-75-75-75v75H1274.61z" />
+        <polygon className="fill-brand-electric-purple" points="199.65,-0.21 -0.33,-0.21 -0.33,199.79 199.65,-0.21" />
+        <polygon className="fill-brand-neutral-400" points="399.72,199.76 399.72,-0.21 199.72,-0.21 399.72,199.76" />
+        <polygon className="fill-brand-neutral-400" points="799.84,199.96 799.84,-0.02 599.84,-0.02 799.84,199.96" />
+        <polygon
+          className="fill-brand-electric-purple"
+          points="799.91,-0.31 799.91,199.67 999.91,199.67 799.91,-0.31"
+        />
+        <path
+          className="fill-brand-neutral-400"
+          d="M1199.75-0.21H999.76v199.99C999.76,89.32,1089.3-0.21,1199.75-0.21z"
+        />
+        <polygon
+          className="fill-brand-neutral-400"
+          points="1599.67,199.96 1599.67,-0.02 1399.67,-0.02 1599.67,199.96"
+        />
+        <path
+          className="fill-brand-iridescent-blue"
+          d="M1199.61,399.96h200v-200C1399.61,310.42,1310.07,399.96,1199.61,399.96z"
+        />
+        <path className="fill-brand-iridescent-blue" d="M1199.61,274.96c41.42,0,75-33.58,75-75h-75V274.96z" />
+        <polygon
+          className="fill-brand-electric-purple"
+          points="1299.62,600.13 1299.62,500.13 1399.61,500.13 1399.61,400.13 1199.61,400.13 1199.61,600.13 1299.62,600.13"
+        />
+        <path
+          className="fill-brand-iridescent-blue"
+          d="M-0.33,599.86h124.99c0-41.42,33.58-74.99,74.99-74.99V399.88C89.21,399.88-0.33,489.42-0.33,599.86z"
+        />
+        <path
+          className="fill-brand-iridescent-blue"
+          d="M-0.33,599.81h124.99c0,41.42,33.58,74.99,74.99,74.99v124.99C89.21,799.79-0.33,710.25-0.33,599.81z"
+        />
+        <polygon className="fill-brand-neutral-400" points="-0.33,399.96 199.65,399.96 199.65,199.96 -0.33,399.96" />
+        <polygon
+          className="fill-brand-iridescent-blue"
+          points="399.72,399.95 399.72,199.97 199.72,199.97 399.72,399.95"
+        />
+        <path
+          className="fill-brand-neutral-400"
+          d="M399.72,399.79h-200v200C199.72,489.34,289.26,399.81,399.72,399.79L399.72,399.79z"
+        />
+        <path
+          className="fill-brand-neutral-400"
+          d="M199.72,599.79h200v-200C399.72,510.23,310.18,599.77,199.72,599.79L199.72,599.79z"
+        />
+        <path
+          className="fill-brand-electric-purple"
+          d="M399.72,799.79v-200h-200C310.16,599.79,399.7,689.33,399.72,799.79L399.72,799.79z"
+        />
+        <path
+          className="fill-brand-electric-purple"
+          d="M199.72,599.79v200h200C289.27,799.79,199.74,710.24,199.72,599.79L199.72,599.79z"
+        />
+        <polygon
+          className="fill-brand-iridescent-blue"
+          points="599.78,799.79 599.78,599.81 399.78,599.81 599.78,799.79 "
+        />
+        <path
+          className="fill-brand-electric-purple"
+          d="M799.84,599.79h-200v200C599.84,689.33,689.39,599.79,799.84,599.79z"
+        />
+        <path className="fill-brand-electric-purple" d="M799.84,724.79c-41.42,0-75,33.58-75,75h75V724.79z" />
+        <polygon
+          className="fill-brand-neutral-400"
+          points="999.91,700.32 899.9,700.32 899.9,800.31 799.91,800.31 799.91,600.31 999.91,600.31 
+			999.91,700.32 "
+        />
+        <path
+          className="fill-brand-neutral-400"
+          d="M1399.67,599.79v200h200C1489.23,799.79,1399.69,710.24,1399.67,599.79L1399.67,599.79z"
+        />
+        <path
+          className="fill-brand-neutral-400"
+          d="M1599.67,799.79v-200h-200C1510.12,599.79,1599.65,689.33,1599.67,799.79L1599.67,799.79z"
+        />
+        <path
+          className="fill-brand-neutral-400"
+          d="M1399.67,399.79v200h200C1489.23,599.79,1399.69,510.25,1399.67,399.79L1399.67,399.79z"
+        />
+        <path
+          className="fill-brand-neutral-400"
+          d="M1599.67,599.79v-200h-200C1510.12,399.79,1599.65,489.33,1599.67,599.79L1599.67,599.79z"
+        />
+        <polygon
+          className="fill-brand-electric-purple"
+          points="1400.33,199.97 1400.33,399.95 1600.33,399.95 1400.33,199.97 		"
+        />
+        <path
+          className="fill-brand-iridescent-blue"
+          d="M1399.6,600.32h-124.99c0,41.42-33.58,74.99-74.99,74.99V800.3
+			C1310.07,800.3,1399.6,710.77,1399.6,600.32z"
+        />
+        <polygon
+          className="fill-brand-electric-purple"
+          points="1199.76,700.32 1099.75,700.32 1099.75,800.31 999.76,800.31 999.76,600.31 
+			1199.76,600.31 1199.76,700.32"
+        />
+      </g>
+      <g>
+        <path
+          className="fill-brand-iridescent-blue"
+          d="M599.37,455.35c-9.93,0-17.08-2.47-21.47-7.41c-4.39-4.94-6.58-12.81-6.58-23.6v-10.62h21.5v13.59
+			c0,2.51,0.38,4.48,1.14,5.9c0.76,1.42,2.09,2.13,3.98,2.13c1.98,0,3.35-0.58,4.11-1.73c0.76-1.15,1.14-3.05,1.14-5.68
+			c0-3.34-0.33-6.13-0.99-8.37c-0.66-2.24-1.8-4.39-3.43-6.42c-1.63-2.04-3.88-4.42-6.76-7.13l-9.76-9.27
+			c-7.29-6.88-10.93-14.74-10.93-23.6c0-9.27,2.15-16.33,6.46-21.19c4.3-4.86,10.53-7.29,18.69-7.29c9.97,0,17.04,2.66,21.22,7.97
+			c4.18,5.31,6.27,13.38,6.27,24.22h-22.11v-7.47c0-1.48-0.42-2.63-1.27-3.46c-0.84-0.82-1.99-1.24-3.43-1.24
+			c-1.73,0-3,0.48-3.8,1.45c-0.8,0.97-1.2,2.21-1.2,3.74c0,1.52,0.41,3.17,1.24,4.94c0.82,1.77,2.45,3.81,4.88,6.12l12.54,12.05
+			c2.51,2.39,4.82,4.91,6.92,7.57s3.79,5.74,5.07,9.27c1.28,3.52,1.91,7.81,1.91,12.88c0,10.21-1.88,18.21-5.65,24
+			C615.28,452.46,608.72,455.35,599.37,455.35z"
+        />
+        <path className="fill-brand-iridescent-blue" d="M726.92,454.36V345.64h21v108.72H726.92z" />
+        <path className="fill-brand-iridescent-blue" d="M850.14,454.36V345.64h21.74v90.19h22.36v18.53H850.14z" />
+        <path
+          className="fill-brand-iridescent-blue"
+          d="M1000.95,455.35c-8.69,0-15.33-2.63-19.92-7.88c-4.59-5.25-6.89-12.84-6.89-22.76v-51.64
+			c0-9.31,2.28-16.37,6.83-21.19c4.55-4.82,11.21-7.23,19.98-7.23s15.43,2.41,19.98,7.23c4.55,4.82,6.83,11.88,6.83,21.19v51.64
+			c0,9.93-2.3,17.51-6.89,22.76C1016.28,452.72,1009.64,455.35,1000.95,455.35z M1001.13,435.33c3.29,0,4.94-3.19,4.94-9.57V372.7
+			c0-5.35-1.61-8.03-4.82-8.03c-3.62,0-5.44,2.74-5.44,8.22v53c0,3.38,0.41,5.8,1.24,7.26
+			C997.88,434.6,999.24,435.33,1001.13,435.33z"
+        />
+        <path
+          className="fill-brand-iridescent-blue"
+          d="M1100.25,524.93h-600.5V275.07h600.5V524.93z M509.67,515.01h580.66V284.99H509.67V515.01z"
+        />
+      </g>
+    </svg>
+  )
+}
+
+SiloLockup.propTypes = {
+  /** Tailwind class names */
+  className: PropTypes.string
+}
+
+export default SiloLockup
--- pages/index.js
+++ pages/index.js
@@ -1,19 +1,23 @@
 import React from 'react'
 
 import CockroachLabsLogo from '../components/cockroach-labs-logo'
+import SiloLockup from '../components/silo-lockup'
 
 const Page = () => {
   return (
-    <section className="flex flex-col gap-2 justify-items-center">
+    <section className="flex flex-col gap-8 justify-items-center">
+      <SiloLockup className="max-w-[800px] h-auto mx-auto" />
       <CockroachLabsLogo color="brand-white" className="w-[160px] mx-auto" />
-      <h1 className="m-0 text-lg text-center">The Art of Data Residency and Application Architecture.</h1>
-      <span className="block text-sm text-center">
-        A{' '}
-        <a className="font-bold" href="https://www.cockroachlabs.com/" target="_blank" rel="noopener">
-          CockroachDB Serverless
-        </a>{' '}
-        demo.
-      </span>
+      <div>
+        <h1 className="m-0 text-lg text-center">The Art of Data Residency and Application Architecture.</h1>
+        <span className="block text-sm text-center">
+          A{' '}
+          <a className="font-bold" href="https://www.cockroachlabs.com/" target="_blank" rel="noopener">
+            CockroachDB Serverless
+          </a>{' '}
+          demo.
+        </span>
+      </div>
     </section>
   )
 }
