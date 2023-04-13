349efe0c5119d63c35bb66a4500d9535e38e449c
Paul Scanlon <pauliescanlon@gmail.com>
2023-04-13 12:57:36 +0100

    style: chage  app layout

--- pages/app.js
+++ pages/app.js
@@ -10,12 +10,12 @@ const Page = () => {
 
   return (
     <section className='flex flex-col gap-16'>
-      <article>
+      <article className='flex flex-col gap-4'>
         <h1 className='heading-lg'>art</h1>
-        <p className='mx-auto max-w-lg text-center'>
+        <p className='mx-auto my-0 max-w-lg text-center'>
           Lorem ipsum dolor sit amet, consectetur adipiscing elit. In a tincidunt nisl, sed interdum ante
         </p>
-        <pre>{JSON.stringify(session, null, 2)}</pre>
+        {/* <pre>{JSON.stringify(session, null, 2)}</pre> */}
       </article>
 
       <article className='flex flex-col gap-4 sm:gap-8 bg-depth-0 border-2 border-depth-2 p-4 sm:p-8'>
@@ -46,26 +46,33 @@ const Page = () => {
           </div>
         </div>
 
-        <div className='flex flex-col gap-16 bg-depth-1 border-2 border-depth-2  p-4 sm:p-8'>
-          <h2 className='m-0 p-0 text-brand-white text-lg sm:text-3xl normal-case tracking-normal font-sans'>Global</h2>
-          <div className='flex flex-col lg:flex-row gap-0 lg:gap-4'>
-            <RegionHeading flag='🇺🇸' country='USA' region='us-east-1 | (N. Virginia)' />
-            <RegionHeading flag='🇺🇸' country='USA' region='us-west-2 | (Oregon)' />
-            <RegionHeading flag='🇩🇪' country='Germany' region='eu-central-1 | (Frankfurt)' />
+        <div className='flex flex-col gap-16 p-4 sm:p-8'>
+          <div className='flex flex-col gap-4'>
+            <h2 className='m-0 p-0 text-brand-white text-lg sm:text-3xl normal-case tracking-normal font-sans'>
+              Global
+            </h2>
+            <div className='flex flex-col lg:flex-row gap-0 lg:gap-4'>
+              <RegionHeading flag='🇺🇸' country='USA' region='us-east-1 | (N. Virginia)' />
+              <RegionHeading flag='🇺🇸' country='USA' region='us-west-2 | (Oregon)' />
+              <RegionHeading flag='🇩🇪' country='Germany' region='eu-central-1 | (Frankfurt)' />
+            </div>
           </div>
         </div>
       </article>
 
-      <article>
+      <article className='flex flex-col gap-4'>
         <h2 className='heading-lg'>data</h2>
-        <div className='flex flex-col gap-4 sm:gap-8 bg-depth-0 border-2 border-depth-2 p-4 sm:p-8'>
-          <AppContext.Consumer>
-            {({ values }) => {
-              return <pre>{JSON.stringify(values, null, 2)}</pre>
-            }}
-          </AppContext.Consumer>
-        </div>
+        <p className='mx-auto my-0 max-w-lg text-center'>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. In a tincidunt nisl, sed interdum ante
+        </p>
       </article>
+      {/* <article>
+        <AppContext.Consumer>
+          {({ values }) => {
+            return <pre>{JSON.stringify(values, null, 2)}</pre>
+          }}
+        </AppContext.Consumer>
+      </article> */}
     </section>
   )
 }
