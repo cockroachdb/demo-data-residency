3880f4eb9fc45245e96639dca14946829c1def89
Paul Scanlon <pauliescanlon@gmail.com>
2023-04-25 19:42:08 +0100

    feat: add checkbox to newsletter form

--- components/newsletter-form.js
+++ components/newsletter-form.js
@@ -8,6 +8,7 @@ import MarketoForm from './marketo-form'
 
 const NewsletterForm = ({ formId }) => {
   const [email, setEmail] = useState('')
+  const [checked, setChecked] = useState(false)
   const [state, dispatch] = useReducer(reducer, initialState)
 
   const handleSubmit = (event) => {
@@ -41,7 +42,7 @@ const NewsletterForm = ({ formId }) => {
           iaculis
         </p>
       </div>
-      <div className='flex flex-col mx-auto w-full sm:max-w-lg gap-1'>
+      <div className='flex flex-col mx-auto w-full sm:max-w-lg gap-2'>
         <form onSubmit={handleSubmit} className='flex gap-2 items-end'>
           <label className='flex flex-col gap-1 grow text-xs text-brand-white font-medium'>
             <span className="after:content-['*'] after:mt-0.5 after:text-red-500 flex gap-1 grow text-xs text-brand-white font-medium">
@@ -62,7 +63,7 @@ const NewsletterForm = ({ formId }) => {
           <button
             type='submit'
             className='text-sm min-w-fit border-brand-electric-purple bg-brand-electric-purple text-brand-white disabled:border-brand-evening-hush disabled:bg-brand-evening-hush disabled:text-brand-white disabled:cursor-not-allowed px-2 transition-color duration-300 hover:text-brand-white hover:border-brand-white'
-            disabled={state.isSubmitting}
+            disabled={state.isSubmitting || !checked}
             aria-label='Newsletter subscribe'
           >
             <svg
@@ -81,22 +82,56 @@ const NewsletterForm = ({ formId }) => {
             </svg>
           </button>
         </form>
+
         <span className='flex items-start py-1 h-6'>
           {state.isSubmitting ? <span className='text-xs text-brand-warning'>Submitting...</span> : null}
           {state.success ? <span className='text-xs text-green-500'>Thanks for signing up.</span> : null}
         </span>
 
-        <small className='block text-xs text-center text-brand-evening-hush'>
-          To update your email preferences visit{' '}
-          <a
-            href='https://www.cockroachlabs.com/email-preferences/'
-            target='_blank'
-            rel='noreferrer'
-            className='text-brand-evening-hush transition-color duration-300 hover:text-brand-white'
-          >
-            cockroachlabs.com
-          </a>
-        </small>
+        <div className='flex flex-col gap-4'>
+          <div className='flex gap-2 items-center justify-center'>
+            <input
+              checked={checked}
+              id='checkbox'
+              type='checkbox'
+              value=''
+              className='w-4 h-4 bg-red-400 focus:ring-0'
+              onChange={() => setChecked(!checked)}
+            />
+            <label htmlFor='checkbox' className='text-xs font-medium text-brand-neutral-400'>
+              I agree to the{' '}
+              <a
+                href='https://www.cockroachlabs.com/cloud-terms-and-conditions/'
+                rel='noopener'
+                target='_blank'
+                className='text-brand-evening-hush transition-color duration-300 hover:text-brand-white'
+              >
+                terms of service
+              </a>{' '}
+              and{' '}
+              <a
+                href='https://www.cockroachlabs.com/privacy/'
+                rel='noopener'
+                target='_blank'
+                className='text-brand-evening-hush transition-color duration-300 hover:text-brand-white'
+              >
+                privacy policy
+              </a>
+            </label>
+          </div>
+
+          <small className='block text-xs text-center text-brand-neutral-400'>
+            To update your email preferences visit{' '}
+            <a
+              href='https://www.cockroachlabs.com/email-preferences/'
+              target='_blank'
+              rel='noreferrer'
+              className='text-brand-evening-hush transition-color duration-300 hover:text-brand-white'
+            >
+              cockroachlabs.com
+            </a>
+          </small>
+        </div>
         <MarketoForm debug={false} formId={formId} />
       </div>
     </section>
