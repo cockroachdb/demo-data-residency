270ed3d0aac7009ffc27d4e3f1a4266e787a06f7
Paul Scanlon <pauliescanlon@gmail.com>
2023-04-25 18:14:50 +0100

    refactor: change render conditions on admin page

--- pages/admin.js
+++ pages/admin.js
@@ -1,8 +1,9 @@
-import React from 'react'
+import React, { Fragment } from 'react'
 import { useSession } from 'next-auth/react'
 import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query'
 
 import LoadingSpinner from '../components/loading-spinner'
+import ErrorMessage from '../components/error-message'
 
 const Page = () => {
   const { data: session } = useSession()
@@ -68,53 +69,57 @@ const Page = () => {
       </div>
       {session?.user.admin ? (
         <div>
+          {query.isError ? <ErrorMessage /> : null}
           {query.status === 'loading' ? (
             <div className='flex justify-center'>
               <LoadingSpinner />
             </div>
-          ) : (
-            <div>
-              <table className='m-0 table-auto'>
-                <thead className='p-3 text-primary font-bold border-depth-2'>
-                  <tr className='text-brand-white'>
-                    <th className='text-inherit'>ID</th>
-                    <th className='text-inherit'>Username</th>
-                    <th className='text-inherit'>Super Region</th>
-                    <th className='text-inherit text-right'>Action</th>
-                  </tr>
-                </thead>
-                <tbody className=''>
-                  {query.data.map((d, index) => {
-                    const { user_id, username, super_region } = d
+          ) : null}
+          {!query.isError && query.status !== 'loading' ? (
+            <Fragment>
+              <div>
+                <table className='m-0 table-auto'>
+                  <thead className='p-3 text-primary font-bold border-depth-2'>
+                    <tr className='text-brand-white'>
+                      <th className='text-inherit'>ID</th>
+                      <th className='text-inherit'>Username</th>
+                      <th className='text-inherit'>Super Region</th>
+                      <th className='text-inherit text-right'>Action</th>
+                    </tr>
+                  </thead>
+                  <tbody className=''>
+                    {query.data.map((d, index) => {
+                      const { user_id, username, super_region } = d
 
-                    return (
-                      <tr key={index} className='border-depth-2 text-brand-neutral-400'>
-                        <td className='text-inherit'>{user_id}</td>
-                        <td className='text-inherit'>{username}</td>
-                        <td className='text-inherit uppercase'>{super_region || 'global'}</td>
-                        <td className='text-inherit text-right'>
-                          <button
-                            className='flex items-center justify-center border-brand-danger text-brand-danger ml-auto min-w-[100px] min-h-[44px]'
-                            disabled={mutation.isLoading && mutation.variables === user_id ? true : false}
-                            onClick={() => {
-                              mutation.mutate(user_id)
-                            }}
-                          >
-                            {mutation.isLoading && mutation.variables === user_id ? (
-                              <LoadingSpinner color='fill-brand-danger' />
-                            ) : (
-                              'Delete'
-                            )}
-                          </button>
-                        </td>
-                      </tr>
-                    )
-                  })}
-                </tbody>
-              </table>
-            </div>
-          )}
-          <pre>{JSON.stringify(query.data, null, 2)}</pre>
+                      return (
+                        <tr key={index} className='border-depth-2 text-brand-neutral-400'>
+                          <td className='text-inherit'>{user_id}</td>
+                          <td className='text-inherit'>{username}</td>
+                          <td className='text-inherit uppercase'>{super_region || 'global'}</td>
+                          <td className='text-inherit text-right'>
+                            <button
+                              className='flex items-center justify-center border-brand-danger text-brand-danger ml-auto min-w-[100px] min-h-[44px]'
+                              disabled={mutation.isLoading && mutation.variables === user_id ? true : false}
+                              onClick={() => {
+                                mutation.mutate(user_id)
+                              }}
+                            >
+                              {mutation.isLoading && mutation.variables === user_id ? (
+                                <LoadingSpinner color='fill-brand-danger' />
+                              ) : (
+                                'Delete'
+                              )}
+                            </button>
+                          </td>
+                        </tr>
+                      )
+                    })}
+                  </tbody>
+                </table>
+              </div>
+              {/* <pre>{JSON.stringify(query.data, null, 2)}</pre> */}
+            </Fragment>
+          ) : null}
         </div>
       ) : null}
     </section>
