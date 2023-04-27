9697dff84a4ee17a8785362c8600257e44047ac2
Paul Scanlon <pauliescanlon@gmail.com>
2023-04-27 16:57:03 +0100

    style: change border color on toggle

--- pages/index.js
+++ pages/index.js
@@ -166,7 +166,7 @@ const Page = () => {
             <div className='flex justify-center'>
               <label className='relative inline-flex items-center mr-5 cursor-pointer'>
                 <input type='checkbox' value={currentRegion} className='sr-only peer' onChange={handleToggle} />
-                <div className="w-11 h-6 bg-brand-danger rounded-full peer peer-focus:ring-2 peer-focus:ring-blue-600  peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-0.5 after:left-[2px] after:bg-white after:border-gray-900 after:border after:rounded-full after:h-5 after:w-5 after:transition-all peer-checked:bg-brand-blue"></div>
+                <div className="w-11 h-6 bg-brand-danger rounded-full peer peer-focus:ring-2 peer-focus:ring-blue-500  peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-0.5 after:left-[2px] after:bg-brand-white after:border-brand-white after:border after:rounded-full after:h-5 after:w-5 after:transition-all peer-checked:bg-brand-blue"></div>
                 <span className='ml-3 text-sm font-medium text-brand-white uppercase'>{currentRegion}</span>
               </label>
             </div>
