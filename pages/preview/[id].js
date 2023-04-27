f8fb449549f80d4ea3665f1b0c4bcca117a82c55
Paul Scanlon <pauliescanlon@gmail.com>
2023-04-27 16:51:11 +0100

    style: make preivew heading bold

--- pages/preview/[id].js
+++ pages/preview/[id].js
@@ -104,7 +104,7 @@ const Page = ({ user_id }) => {
               <div className='flex flex-col gap-4'>
                 <h1 className='heading-lg'>preview</h1>
                 <div className='flex flex-col gap-2 mx-auto max-w-lg'>
-                  <p className='flex gap-2 m-0 text-center'>
+                  <p className='flex gap-2 m-0 text-center font-bold'>
                     Art by <strong className='text-brand-pink'>{data.results.username}</strong>
                     <time className='text-inherit'>{`| ${data.results.local_last_update}`}</time>
                   </p>
