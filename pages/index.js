20bce3ff754d1f2d1811506621453d20ae60a68c
Paul Scanlon <pauliescanlon@gmail.com>
2023-04-28 21:06:25 +0100

    style: change grid gaps on home page

--- pages/index.js
+++ pages/index.js
@@ -27,21 +27,21 @@ const Page = () => {
     <div className='flex flex-col gap-24 sm:gap-48 mx-auto max-w-6xl'>
       <section className='flex flex-col gap-8 justify-items-center'>
         <SiloLockup className='max-w-4xl h-auto mx-auto' />
-        <div className='flex flex-col gap-4'>
-          <h1 className='m-0 text-brand-white text-3xl md:text-6xl text-center normal-case font-sans md:leading-tight'>
+        <div className='flex flex-col gap-4 mx-auto max-w-4xl'>
+          <h1 className='m-0 text-brand-white text-3xl md:text-5xl text-center normal-case font-sans md:leading-tight'>
             The Art of Data Residency and Application Architecture.
           </h1>
-          <h2 className='m-0 text-brand-white text-2xl text-center normal-case tracking-normal font-sans'>
+          <h2 className='m-0 text-brand-gray-b text-lg md:text-2xl font-normal text-center normal-case tracking-normal font-sans'>
             A demo of multi-region capabilities in CockroachDB.
           </h2>
         </div>
       </section>
 
       <section className='flex flex-col gap-8'>
-        <div className='flex flex-col gap-8'>
+        <div className='flex flex-col gap-4 md:gap-8'>
           <h2 className='heading-md'>why</h2>
           <div className='flex flex-col gap-4 text-center mx-auto max-w-3xl'>
-            <p>
+            <p className='m-0'>
               When your application serves users across states, countries, or continents, you need to deploy your
               database in multiple cloud regions. But setting up and managing a multi-region database can be
               labor-intensive and expensive.
@@ -75,7 +75,7 @@ const Page = () => {
       </section>
 
       <section className='flex flex-col gap-8'>
-        <div className='flex flex-col gap-8'>
+        <div className='flex flex-col gap-4 md:gap-8'>
           <h2 className='heading-md'>make your own art</h2>
           <div className='flex flex-col gap-8 text-center mx-auto max-w-4xl'>
             <p className='m-0'>
@@ -98,7 +98,7 @@ const Page = () => {
       </section>
 
       <section className='flex flex-col gap-8'>
-        <div className='flex flex-col gap-8'>
+        <div className='flex flex-col gap-4 md:gap-8'>
           <h2 className='heading-md'>view the gallery</h2>
           <div className='flex flex-col gap-8 text-center mx-auto max-w-4xl'>
             <p className='m-0'>
@@ -134,8 +134,8 @@ const Page = () => {
         </div>
       </section>
 
-      <section className='flex flex-col gap-36 xl:gap-24'>
-        <div className='flex flex-col gap-8'>
+      <section className='flex flex-col gap-8 xl:gap-24'>
+        <div className='flex flex-col gap-4 md:gap-8'>
           <h2 className='heading-md'>global data</h2>
           <div className='flex flex-col gap-8 text-center mx-auto max-w-4xl'>
             <p className='m-0 '>
@@ -154,7 +154,7 @@ const Page = () => {
           </div>
         </div>
         <div className='relative'>
-          <div className='relative bg-brand-deep-purple mx-auto overflow-auto max-h-[300px] xl:max-h-max xl:overflow-visible border-2 border-depth-3 w-full max-w-4xl shadow-3xl z-10'>
+          <div className='relative bg-brand-deep-purple mx-auto overflow-auto max-h-[450px] xl:max-h-max xl:overflow-visible border-2 border-depth-3 w-full max-w-4xl shadow-3xl z-10'>
             <table className='m-0 border-collapse'>
               <thead className='bg-depth-0 border-b-2 border-b-dashed border-b-depth-3'>
                 <tr>
@@ -197,7 +197,7 @@ const Page = () => {
                           ) : null}
                         </div>
                       </td>
-                      <td className={`flex items-center gap-1 table-blur-${isActive}`}>
+                      <td className={`flex items-center gap-1 whitespace-nowrap table-blur-${isActive}`}>
                         <span className='mt-1'>{emoji}</span>
                         <span>{region}</span>
                       </td>
@@ -214,15 +214,17 @@ const Page = () => {
       </section>
 
       <section className='grid md:grid-cols-2 gap-16 items-center mx-auto max-w-4xl'>
-        <div className='flex flex-col gap-8'>
+        <div className='flex flex-col gap-4 md:gap-8'>
           <h2 className='md:text-left md:pl-0 heading-md'>united states data</h2>
-          <p className='m-0 text-center md:text-left'>
-            Certain images, colors, and shapes in the demo are restricted to the <b>United States</b> and will never be
-            available outside of it. If you're located anywhere in the world except <b>Europe</b>, you will only be able
-            to see <b>United States</b> data in the gallery. The data is replicated between CockroachDB instances on the
-            East coast and West coast to provide high-availability and guard against regional outages
-          </p>
-          <TryDemoLink />
+          <div className='flex flex-col gap-8 text-center mx-auto max-w-4xl'>
+            <p className='m-0 text-center md:text-left'>
+              Certain images, colors, and shapes in the demo are restricted to the <b>United States</b> and will never
+              be available outside of it. If you're located anywhere in the world except <b>Europe</b>, you will only be
+              able to see <b>United States</b> data in the gallery. The data is replicated between CockroachDB instances
+              on the East coast and West coast to provide high-availability and guard against regional outages
+            </p>
+            <TryDemoLink />
+          </div>
         </div>
         <div className='relative flex items-center justify-center translate-x-0'>
           <Image src={usDots} alt='us data' width={500} height={500} />
@@ -237,14 +239,16 @@ const Page = () => {
       </section>
 
       <section className='grid md:grid-cols-2 gap-16 items-center mx-auto max-w-4xl'>
-        <div className='flex flex-col gap-8 md:order-1'>
+        <div className='flex flex-col md:order-1 gap-4 md:gap-8'>
           <h2 className='md:text-right heading-md'>european data</h2>
-          <p className='m-0 text-center md:text-right'>
-            Certain images, colors, and shapes in the demo are restricted to <b>Europe</b> and will never be available
-            outside of it. If you're located in <b>Europe</b>, you will only be able to see European data. Given the
-            stringent European data privacy regulations, this capability is especially important.
-          </p>
-          <TryDemoLink className='self-center md:self-end' />
+          <div className='flex flex-col gap-8 text-center mx-auto max-w-4xl'>
+            <p className='m-0 text-center md:text-right'>
+              Certain images, colors, and shapes in the demo are restricted to <b>Europe</b> and will never be available
+              outside of it. If you're located in <b>Europe</b>, you will only be able to see European data. Given the
+              stringent European data privacy regulations, this capability is especially important.
+            </p>
+            <TryDemoLink className='self-center md:self-end' />
+          </div>
         </div>
         <div className='relative flex items-center justify-center translate-x-0'>
           <Image src={euDots} alt='us data' width={500} height={500} />
@@ -259,35 +263,35 @@ const Page = () => {
       </section>
 
       <section className='flex flex-col gap-8'>
-        <div className='flex flex-col gap-8'>
+        <div className='flex flex-col gap-4 md:gap-8'>
           <h2 className='heading-md'>learn how it works</h2>
-          <div className='flex flex-col gap-4 text-center mx-auto max-w-2xl'>
+          <div className='flex flex-col gap-8 text-center mx-auto max-w-2xl'>
             <p className='m-0 '>We've written an in-depth blog post about how the demo works. Check it out!</p>
+            <div className='mx-auto'>
+              <a
+                href='https://www.cockroachlabs.com/blog'
+                target='_blank'
+                rel='noopener'
+                className='flex gap-2 items-center self-center px-4 py-2 border-2 border-brand-iridescent-blue text-brand-iridescent-blue capitalize no-underline transition-color duration-300 hover:text-brand-white hover:border-brand-white'
+              >
+                <svg
+                  xmlns='http://www.w3.org/2000/svg'
+                  fill='none'
+                  viewBox='0 0 24 24'
+                  strokeWidth={1.5}
+                  stroke='currentColor'
+                  className='w-5 h-5'
+                >
+                  <path
+                    strokeLinecap='round'
+                    strokeLinejoin='round'
+                    d='M19.5 14.25v-2.625a3.375 3.375 0 00-3.375-3.375h-1.5A1.125 1.125 0 0113.5 7.125v-1.5a3.375 3.375 0 00-3.375-3.375H8.25m0 12.75h7.5m-7.5 3H12M10.5 2.25H5.625c-.621 0-1.125.504-1.125 1.125v17.25c0 .621.504 1.125 1.125 1.125h12.75c.621 0 1.125-.504 1.125-1.125V11.25a9 9 0 00-9-9z'
+                  />
+                </svg>
+                read post
+              </a>
+            </div>
           </div>
-        </div>
-        <div className='mx-auto'>
-          <a
-            href='https://www.cockroachlabs.com/blog'
-            target='_blank'
-            rel='noopener'
-            className='flex gap-2 items-center self-center px-4 py-2 border-2 border-brand-iridescent-blue text-brand-iridescent-blue capitalize no-underline transition-color duration-300 hover:text-brand-white hover:border-brand-white'
-          >
-            <svg
-              xmlns='http://www.w3.org/2000/svg'
-              fill='none'
-              viewBox='0 0 24 24'
-              strokeWidth={1.5}
-              stroke='currentColor'
-              className='w-5 h-5'
-            >
-              <path
-                strokeLinecap='round'
-                strokeLinejoin='round'
-                d='M19.5 14.25v-2.625a3.375 3.375 0 00-3.375-3.375h-1.5A1.125 1.125 0 0113.5 7.125v-1.5a3.375 3.375 0 00-3.375-3.375H8.25m0 12.75h7.5m-7.5 3H12M10.5 2.25H5.625c-.621 0-1.125.504-1.125 1.125v17.25c0 .621.504 1.125 1.125 1.125h12.75c.621 0 1.125-.504 1.125-1.125V11.25a9 9 0 00-9-9z'
-              />
-            </svg>
-            read post
-          </a>
         </div>
       </section>
 
