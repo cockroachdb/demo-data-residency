d1982449667794cd3fe966a5d3bb2320e19afdfd
Paul Scanlon <pauliescanlon@gmail.com>
2023-05-05 13:06:33 +0100

    feat: add how it works diagram

--- pages/index.js
+++ pages/index.js
@@ -5,6 +5,7 @@ import { YouTubeLite } from 'react-youtube-lite'
 
 import SiloLockup from '../components/silo-lockup'
 import NewsletterForm from '../components/newsletter-form'
+import TryDemoLink from '../components/try-demo-link'
 
 import howItWorksDots from '../public/images/how-it-works-dots.svg'
 import usDots from '../public/images/us-dots.svg'
@@ -13,7 +14,7 @@ import galleryGraphic from '../public/images/gallery-graphic.jpg'
 import usGraphic from '../public/images/us-graphic.jpg'
 import euDots from '../public/images/eu-dots.svg'
 import euGraphic from '../public/images/eu-graphic.jpg'
-import TryDemoLink from '../components/try-demo-link'
+import diagram from '../public/images/how-it-works-diagram_v4.svg'
 
 import globalTable from '../public/global-table.json'
 
@@ -97,8 +98,8 @@ const Page = () => {
               individual rows.
             </p>
             <p className='m-0'>
-              In this demo, some data is restricted either to the <b>United States</b> or <b>Europe</b>, while other
-              data is available in both places.
+              In this demo, some data is stored in either the <b>United States</b> or <b>Europe</b>, while other data is
+              stored in both places.
             </p>
             <TryDemoLink className='self-start' />
           </div>
@@ -164,12 +165,12 @@ const Page = () => {
           <h2 className='text-left md:text-center heading-md'>global data</h2>
           <div className='flex flex-col gap-8 text-center'>
             <p className='m-0 text-left md:text-center'>
-              The artwork's pattern and logo settings are available globally. The data is replicated across all three
-              regions in the CockroachDB cluster. This setting is useful for application data that's universally
-              relevant.
+              The artwork's pattern and logo settings are stored in both the United States and Europe, and available for
+              viewing globally. The data is replicated across all three regions in the CockroachDB cluster. This setting
+              is useful for application data that's universally relevant.
             </p>
             <p className='m-0 text-left md:text-center'>
-              Use the toggle below to understand how the gallery data is represented in CockroachDB
+              Use the toggle below to understand how the gallery data is represented in CockroachDB.
             </p>
             <div className='flex md:justify-center'>
               <label className='relative inline-flex items-center mr-5 cursor-pointer'>
@@ -246,10 +247,10 @@ const Page = () => {
           <h2 className='text-left md:pl-0 heading-md'>united states data</h2>
           <div className='flex flex-col gap-8'>
             <p className='m-0 text-left'>
-              Certain images, colors, and shapes in the demo are restricted to the <b>United States</b> and will never
-              be available outside of it. If you're located anywhere in the world except <b>Europe</b>, you will only be
-              able to see <b>United States</b> data in the gallery. The data is replicated between CockroachDB instances
-              on the East coast and West coast to provide high-availability and guard against regional outages
+              Certain images, colors, and shapes in the demo are stored in the <b>United States</b>. If you're located
+              anywhere in the world except <b>Europe</b> you can only see <b>United States</b> data in the gallery. The
+              data is replicated between CockroachDB instances on the East coast and West coast to provide
+              high-availability and guard against regional outages.
             </p>
             <TryDemoLink className='self-start' />
           </div>
@@ -271,9 +272,9 @@ const Page = () => {
           <h2 className='text-left md:text-right heading-md'>european data</h2>
           <div className='flex flex-col gap-8'>
             <p className='m-0 text-left md:text-right'>
-              Certain images, colors, and shapes in the demo are restricted to <b>Europe</b> and will never be available
-              outside of it. If you're located in <b>Europe</b>, you will only be able to see European data. Given the
-              stringent European data privacy regulations, this capability is especially important.
+              Certain images, colors, and shapes in the demo are stored in <b>Europe</b> and will never be available
+              outside of it. If you're located in <b>Europe</b>, you can only see European data. Given the stringent
+              European data privacy regulations, this capability is especially important.
             </p>
             <TryDemoLink className='self-start md:self-end' />
           </div>
@@ -292,13 +293,17 @@ const Page = () => {
 
       <section className='flex flex-col gap-8'>
         <div className='flex flex-col gap-4 md:gap-8'>
-          <h2 className='text-left md:text-center heading-md'>learn how it works</h2>
+          <h2 className='text-center heading-md'>learn how it works</h2>
+          <div className='pt-4 mx-auto max-w-5xl'>
+            <Image src={diagram} alt='how it works diagram' width={1400} height={320} className='m-0' />
+          </div>
           <div className='flex flex-col gap-8 md:mx-auto max-w-2xl'>
-            <p className='m-0 text-left md:text-center'>
+            <p className='m-0 text-center'>
               We've written an in-depth blog post explaining how we used AWS multi-region application architecture
               together with CockroachDB Serverless to make this demo.
             </p>
-            <div className='mr-auto md:mx-auto'>
+
+            <div className='mx-auto'>
               <a
                 href='https://www.cockroachlabs.com/blog'
                 target='_blank'
