288641176c1c3402edb538e0266f18111dc4b625
Paul Scanlon <pauliescanlon@gmail.com>
2023-05-12 18:37:18 +0100

    style: change code highlight background color

--- components/code-highlight.js
+++ components/code-highlight.js
@@ -6,7 +6,7 @@ const CodeHighlight = ({ codeBlock, language, showLineNumbers }) => {
   return (
     <article className='relative flex flex-col '>
       <div className='absolute right-0 -top-10 flex justify-end text-xs uppercase'>
-        <div className='flex gap-2 items-center bg-depth-0 p-3 border-t-2 border-l-2 border-r-2 border-depth-2'>
+        <div className='flex gap-2 items-center bg-brand-deep-purple p-3 border-t-2 border-l-2 border-r-2 border-depth-2'>
           <svg
             xmlns='http://www.w3.org/2000/svg'
             fill='none'
@@ -38,7 +38,7 @@ const CodeHighlight = ({ codeBlock, language, showLineNumbers }) => {
             borderRadius: '0px',
             border: '2px solid #271d3f',
             padding: '1rem',
-            background: '#1e1437!important',
+            background: '#190f33!important',
             fontSize: '0.85rem'
           }}
         />
