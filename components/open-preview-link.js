4689f58a1377c0e59b94f789ec43b14e1aa0ce37
Paul Scanlon <pauliescanlon@gmail.com>
2023-05-12 16:07:33 +0100

    style: add select-non to open preivew link

--- components/open-preview-link.js
+++ components/open-preview-link.js
@@ -13,14 +13,14 @@ const OpenPreviewLink = ({ session, disabled }) => {
             d='M13.5 6H5.25A2.25 2.25 0 003 8.25v10.5A2.25 2.25 0 005.25 21h10.5A2.25 2.25 0 0018 18.75V10.5m-10.5 6L21 3m0 0h-5.25M21 3v5.25'
           />
         </svg>
-        open preview
+        Open Preview
       </Fragment>
     )
   }
 
   if (!session || disabled) {
     return (
-      <span className='link-button flex gap-2 items-center border-2 border-brand-evening-hush text-brand-evening-hush capitalize cursor-not-allowed font-medium'>
+      <span className='link-button flex gap-2 items-center border-2 border-brand-evening-hush text-brand-evening-hush cursor-not-allowed select-none font-medium'>
         {childElements()}
       </span>
     )
@@ -28,7 +28,7 @@ const OpenPreviewLink = ({ session, disabled }) => {
 
   return (
     <Link
-      className='link-button flex gap-2 items-center border-2 no-underline border-brand-pink text-brand-pink capitalize transition-color duration-300 hover:border-brand-white hover:text-brand-white'
+      className='link-button flex gap-2 items-center border-2 no-underline border-brand-pink text-brand-pink transition-color duration-300 hover:border-brand-white hover:text-brand-white'
       href={`/preview/${session.user.id}`}
     >
       {childElements()}
