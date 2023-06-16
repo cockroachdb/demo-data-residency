06ecfe088f6ccbdd5254c295fe5e6394be8b9f63
Paul Scanlon <pauliescanlon@gmail.com>
2023-06-16 11:21:43 -0400

    docs: update messaging from Mr Steve

--- components/sign-in-link.js
+++ components/sign-in-link.js
@@ -7,7 +7,7 @@ const SignInLink = ({ onClick }) => {
   return (
     <Link
       href='/sign-in'
-      className='border-2 px-4 py-2 text-brand-pink text-center border-brand-pink transition-color duration-300 hover:text-brand-white hover:border-brand-white w-full'
+      className='border-2 px-4 py-2 text-brand-pink text-center border-brand-pink transition-color duration-300 hover:text-brand-white hover:border-brand-white w-full no-underline'
       onClick={onClick}
     >
       Sign in
